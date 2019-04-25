f15815a55c55c59c798139fbfd382dff13443d6f
Rubentba <rubenatorresbeltran@gmail.com>
2019-04-25 05:24:19 -0400

    Initialieze Firebase.

---
+++ javascript/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+apiKey: "AIzaSyCaxe0eZx7b0jCfP8M02VP8h9G-uppDrG0",
+authDomain: "trainscheduler-926e7.firebaseapp.com",
+databaseURL: "https://trainscheduler-926e7.firebaseio.com",
+projectId: "trainscheduler-926e7",
+storageBucket: "trainscheduler-926e7.appspot.com",
+messagingSenderId: "1054203526657"
+};
+firebase.initializeApp(config);
